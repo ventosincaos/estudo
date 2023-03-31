2002bf563bf9589884ec7ee191d63cf529f45533
— mari <111576277+ventosincaos@users.noreply.github.com>
2023-03-31 09:35:12 -0300

    Upload 1

---
+++ java/java-faculdade/ex1.java
@@ -0,0 +1,17 @@
+
+public class ex1 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 50;
+		int c = 200;
+		
+		int sum = a + b;
+		
+		if (sum < c) {
+			System.out.println("a soma de A e B é menor que a variavel C");
+		} else {
+			System.out.println("a soma de A e B é maior que a variavel C");
+		}
+	}
+
+}
