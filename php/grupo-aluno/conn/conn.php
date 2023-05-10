2ce7bb88e29f3c1f1457ab4f45caf7d0fb7ae8a8
— mari <111576277+ventosincaos@users.noreply.github.com>
2023-05-10 19:52:18 -0300

    update

---
+++ php/grupo-aluno/conn/conn.php
@@ -0,0 +1,10 @@
+<?php
+try {
+    #cria conexão com o bd e armazena as configurações
+    $PDO = new PDO("mysql:dbname=aluno;host=localhost:3306", "root", "Rosa0107@Diaz");
+    //echo 'conexão ok <br>';
+} catch (PDOException $e) {
+    echo "error: com banco de dados" . $e->getMessage();
+} catch (Exception $e) {
+    echo "error generico" . $e->getMessage();;
+}
